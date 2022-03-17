a87283e52fda85088895b126e4f1d12796490b3c
GIT-KINDMAN <mankind1990s@gmail.com>
2022-03-17 17:58:56 +0900

    22.03.17

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -1,3 +1,31 @@
 /*main.js*/
+const searchEl = document.querySelector('.search');
+const searchInputEl = document.querySelector('input');
 
-console.log('success!')
+searchEl.addEventListener('click', function() {
+    searchInputEl.focus();
+    /* serachEl.focus(); */
+});
+
+searchInputEl.addEventListener('focus',function() {
+    searchInputEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder','통합 검색');
+});
+
+
+searchInputEl.addEventListener('blur',function() {
+    searchInputEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder','');
+});
+
+/*
+searchEl.addEventListener('focus',function() {
+    searchEl.classList.remove('focused');
+    searchEl.setAttribute('placeholder','');
+});
+
+searchEl.addEventListener('blur',function() {
+    searchEl.classList.add('focused');
+    searchEl.setAttribute('placeholder','search');
+});
+*/
